bb841030ce8877db12e907ef53bbb6b4845bcac8
Huniiiiiii <rtg8464@gmail.com>
2025-03-17 02:22:30 +0900

    [feat] 4단계까지 제작 완료

--- src/main/java/mission/Application.java
+++ src/main/java/mission/Application.java
@@ -1,7 +1,63 @@
 package mission;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         //Todo: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("별찍기 단계(1~9)를 입력해주세요.");
+        int Stage = scanner.nextInt(); //Stage 입력
+
+        System.out.print("별찍기 크기를 입력해주세요.");
+        int Size = scanner.nextInt(); //Size 입력
+
+        if (Stage == 1) {
+            for (int i = 1; i <= Size; i++){
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        else if (Stage == 2) {
+            for (int i = Size; i > 0; i--){
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        else if (Stage == 3) {
+            for (int i = Size; i > 0; i--){
+                for (int k = Size; k > i; k--){
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2*i-1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        else if (Stage == 4) {
+            for (int i = 1; i < Size; i++){
+                for (int k = Size; k >= i; k--){
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2*i-1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for (int i = Size; i >= 1; i--){
+                for (int k = Size; k >= i; k--){
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2*i-1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
     }
 }
