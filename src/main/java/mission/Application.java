02c36cd0a8a2fa8adbd5b4190e13ea9a0db604b6
Huniiiiiii <rtg8464@gmail.com>
2025-03-17 11:05:44 +0900

    [feat] 5, 6단계 완성

--- src/main/java/mission/Application.java
+++ src/main/java/mission/Application.java
@@ -59,5 +59,40 @@ public class Application {
                 System.out.println();
             }
         }
+        else if (Stage == 5) {
+            for (int n = 0; n < 2*Size; n++) { //세로줄 반복
+                if(n%2 == 0) { //세로가 짝수 일때
+                    for (int i = 1; i <= Size; i++) {
+                        if (i % 2 != 0) { //가로가 홀수 일때
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                }
+                else { //세로가 홀수 일때
+                    for (int i = 1; i <= Size; i++) {
+                        if (i % 2 != 0) { //가로가 짝수 일때
+                            System.out.print(" ");
+                        } else {
+                            System.out.print("*");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        }
+        else if (Stage == 6) {
+            for (int n = 0; n < Size; n++){
+                for (int k = Size; k > n; k--){
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <= n; j++) {
+                    System.out.print("*");
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
